b2209ace4047f7f37bb5b2f121a8af05985e989d
kollu <venkatkollu23@gmail.com>
2024-02-13 09:06:35 +0530

    Stash testing

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,7 +6,21 @@ public class HelloWorld{
     }
     public String demoMerge(){
 
+
         System.out.pritnln(" demo merge");
+        return  "";
+    }
+
+
+    public String sayHi(){
+        HelloWorldService hs = new HelloWorldService();
+       String studentData = hs.getData();
+
+        if (studentData != null){
+            return "Stash changes";
+
+        }
+
     }
 
 }
